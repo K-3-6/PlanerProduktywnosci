b4b43f6af8294af0ca403eef16b1bce78c853c70
K-3-6 <oskar.k3648@gmail.com>
2026-05-21 20:02:44 +0200

    feat(backend): dodanie kontrolera TasksController z operacjami CRUD

---
+++ PlanerAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlanerAPI.Models;
+
+namespace PlanerAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Ta właściwość reprezentuje naszą tabelę w bazie danych
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
